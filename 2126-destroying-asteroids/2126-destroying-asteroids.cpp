8d3c8f5acc7fb5f8bd6ac68f9c3179e1ad6ba654
Nitin Bhandari <nitinbhandari912@gmail.com>
2026-05-31 08:39:34 +0530

    Time: 0 ms (100%), Space: 106.7 MB (81.61%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        long long sum=mass;
-        sort(asteroids.begin(),asteroids.end());
         int n=asteroids.size();
+        long long int curmass=mass,count=0;
         for(int i=0;i<n;i++){
-            if(sum>=asteroids[i]){
-                sum+=(long long)asteroids[i];
+            if(curmass>=asteroids[i]){
+                curmass+=asteroids[i];
+                asteroids[i]=0;
             }
-            else if(asteroids[i]>sum) return false;
+            else count+=1;
+        }
+        while(count>0){
+            int temp=0;
+            for(int i=0;i<n;i++){
+                if(curmass>=asteroids[i]){
+                    curmass+=asteroids[i];
+                    asteroids[i]=0;
+                }
+                else temp+=1;
+            }
+            if(count==temp) return false;
+            count=temp;
         }
         return true;
     }
