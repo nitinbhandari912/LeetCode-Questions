22ef6172cd30ab5e7598fc6a70daedd3cfea6244
Nitin Bhandari <nitinbhandari912@gmail.com>
2026-07-23 23:57:26 +0530

    Time: 0 ms (100%), Space: 8.5 MB (12.18%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        double kelvin = 273.15 + celsius ;
+        double fahrenheit = celsius*1.80 + 32.00 ;
+        return {kelvin, fahrenheit};
+    }
+};
